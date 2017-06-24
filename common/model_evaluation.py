e54a7850c0f94e4346fbe48f8c1525611a2d4c36
李寅威 <251469031@qq.com>
2017-06-24 09:49:37 +0800

    add multi step prediction evaluation

--- common/model_evaluation.py
+++ common/model_evaluation.py
@@ -8,30 +8,72 @@
 @Description: 模型评估
 """
 
+import pandas as pd
 from sklearn.metrics import explained_variance_score, mean_absolute_error, mean_squared_error, median_absolute_error, \
     r2_score
 
 
-def model_evaluation(actual, predict, fed_data):
-    print("explained_variance_score: " + str(explained_variance_score(actual, predict)))
-    print("mean_absolute_error: " + str(mean_absolute_error(actual, predict)))
-    print("mean_squared_error: " + str(mean_squared_error(actual, predict)))
-    print("median_absolute_error: " + str(median_absolute_error(actual, predict)))
-    print("r2_score: " + str(r2_score(actual, predict)))
-
-    # 趋势正确性评估（价格预测准确率高不等价于趋势预测正确率高，取消该指标）
-    # _calc_trend_accuracy(predict, fed_data)
-
-    # 平均相对误差（Mean Relative Error）
-    _calc_mre(actual, predict)
+def model_evaluation(y_true, y_pred):
+    metrics = _cal_metrics(y_true, y_pred)
+    for (k, v) in metrics.items():
+        print(k + ": " + str(v))
 
 
-def _calc_mre(actual, predict):
+def model_evaluation_multi_step(y_true, y_pred):
+    print(y_true.shape, y_pred.shape)
+    # y_true.shape = (1010, 20)
+    metrics_list = []
+    # 分别求t + 1, t + 2, ... , t + STEP_LEN 每天的评估指标
+    for i in range(len(y_true.T)):
+        metrics = _cal_metrics(pd.DataFrame(y_true.T[i]), pd.DataFrame(y_pred.T[i]))
+        metrics_list.append(metrics)
+        print("-------------------------------------------------------------------------")
+        print("metrics of t + " + str(i + 1))
+        print("-------------------------------------------------------------------------")
+        for (k, v) in metrics.items():
+            print("\r" + k + ": " + str(v))
+
+    # 求总体平均
+    for i in range(1, len(metrics_list)):
+        for (k, v) in metrics_list[0].items():
+            metrics_list[0][k] += metrics_list[i][k]
+    print("-------------------------------------------------------------------------")
+    print("metrics of avg")
+    print("-------------------------------------------------------------------------")
+    for (k, v) in metrics_list[0].items():
+        print("\r" + k + ": " + str(float(v) / y_true.shape[1]))
+
+
+def _cal_metrics(y_true, y_pred):
     """
-    计算平均相对误差（Mean Relative Error）
+    计算各个指标的值
     """
-    print("sum relative error: " + str(((actual - predict) / predict).sum().values))
-    print("mean relative error: " + str(((actual - predict) / predict).mean().values))
+    re = _calc_re(y_true, y_pred)
+    metrics = {
+        "explained_variance_score":
+            explained_variance_score(y_true, y_pred),
+        "mean_absolute_error":
+            mean_absolute_error(y_true, y_pred),
+        "mean_squared_error":
+            mean_squared_error(y_true, y_pred),
+        "median_absolute_error":
+            median_absolute_error(y_true, y_pred),
+        "r2_score":
+            r2_score(y_true, y_pred),
+        "sum_relative_error":
+            re[0],
+        "mean_relative_error":
+            re[1]
+    }
+
+    return metrics
+
+
+def _calc_re(y_true, y_pred):
+    """
+    计算相对误差（Sum/Mean Relative Error）
+    """
+    return [((y_true - y_pred) / y_pred).sum().values, ((y_true - y_pred) / y_pred).mean().values]
 
 
 def _calc_trend_accuracy(predict, fed_data):
