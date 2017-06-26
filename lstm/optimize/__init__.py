9eaf7dea71df4cc720a6b00cc86e612468c66699
李寅威 <251469031@qq.com>
2017-06-26 19:01:27 +0800

    add optimize document

---
+++ lstm/optimize/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Author: liyinwei
+@E-mail: coridc@foxmail.com
+@Time: 2017/6/26 18:55
+@Description: TODO
+"""
+
+if __name__ == '__main__':
+    pass
